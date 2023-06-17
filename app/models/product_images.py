3b38753efc5f6797ce041b907d8036f8c699fc17
KatiePee <piele.katie@gmail.com>
2023-06-17 15:49:09 -0700

    added to git ignore

--- app/models/product_images.py
+++ app/models/product_images.py
@@ -3,6 +3,9 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class ProductImages(db.Model):
     __tablename__ = 'product_images'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+        
     id = db.Column(db.Integer, primary_key=True)
     image_url = db.Column(db.String(255), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('products.id')))
