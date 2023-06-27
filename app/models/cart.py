561acd00566c6a94a4b5896452727d0cf012fa5d
KatiePee <piele.katie@gmail.com>
2023-06-27 12:38:22 -0700

    cart and delete user bug

---
+++ app/models/cart.py
@@ -0,0 +1,30 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+
+    if environment == 'production':
+      __table_args__ = { 'schema': SCHEMA }
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+    total = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+
+    cart_items = db.relationship('Cart_Items', back_populates='cart')
+    user = db.relationship('User', back_populates='cart')
+   
+
+    def to_dict(self):
+      return {
+      'id': self.id,
+      'userId': self.user_id,
+      'total': self.total,
+      'cart': [cart_item.to_dict() for cart_item in self.cart_items],
+      'createdAt': self.created_at 
+      }
+    
+
+
--- app/models/product.py
+++ app/models/product.py
@@ -17,9 +17,9 @@ class Product(db.Model):
     user = db.relationship('User', back_populates='products')
     reviews = db.relationship('Review', back_populates='product', cascade="all, delete-orphan")
     wishlist = db.relationship('User', secondary="wishlist",  back_populates="wishlist", cascade="all, delete")
-
     product_images = db.relationship('ProductImages', back_populates='product', cascade='all, delete-orphan')
-    
+    # cart_items = db.relationship('Cart_Item', back_populates='product')
+
     def to_dict(self):
         return {
             'id': self.id,
@@ -27,5 +27,13 @@ class Product(db.Model):
             "title": self.title,
             "description": self.description,
             "price": self.price,
+            # "images": [image.to_dict() for image in self.product_images],
+            # "reviews": [review.to_dict() for review in self.reviews],
+            # "user": self.user.to_dict(),
             "createdAt": self.created_at
         }
+    
+    #~~~~~~~~~~~~~~~~ left off ~~~~~~~~~~~~~~~~~~
+    # refactored the return of products ti include all nessisary information that is being returned 
+    # todo - refactor routes to utilize this more simple refactoring
+    #fixing the delete user bug
