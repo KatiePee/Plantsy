381ce7ea99a5fee010205b23d10aa763a7bfe1c0
KatiePee <piele.katie@gmail.com>
2023-06-27 12:44:18 -0700

    i think i fixed the bug

--- app/models/product.py
+++ app/models/product.py
@@ -16,7 +16,9 @@ class Product(db.Model):
 
     user = db.relationship('User', back_populates='products')
     reviews = db.relationship('Review', back_populates='product', cascade="all, delete-orphan")
-    wishlist = db.relationship('User', secondary="wishlist",  back_populates="wishlist", cascade="all, delete")
+    # wishlist = db.relationship('User', secondary="wishlist",  back_populates="wishlist", cascade="all, delete-orphan")
+    wishlist = db.relationship('User', secondary="wishlist",  back_populates="wishlist")
+
     product_images = db.relationship('ProductImages', back_populates='product', cascade='all, delete-orphan')
     # cart_items = db.relationship('Cart_Item', back_populates='product')
 
@@ -37,3 +39,5 @@ class Product(db.Model):
     # refactored the return of products ti include all nessisary information that is being returned 
     # todo - refactor routes to utilize this more simple refactoring
     #fixing the delete user bug
+    #bug notes
+#sqlalchemy.exc.InvalidRequestError: One or more mappers failed to initialize - can't proceed with initialization of other mappers. Triggering mapper: 'mapped class User->users'. Original exception was: For many-to-many relationship User.wishlist, delete-orphan cascade is normally configured only on the "one" side of a one-to-many relationship, and not on the "many" side of a many-to-one or many-to-many relationship.  To force this relationship to allow a particular "Product" object to be referred towards by only a single "User" object at a time via the User.wishlist relationship, which would allow delete-orphan cascade to take place in this direction, set the single_parent=True flag. (Background on this error at: https://sqlalche.me/e/14/bbf0)
