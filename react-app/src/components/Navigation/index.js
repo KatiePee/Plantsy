fa1a0174e77ab1a5194a6eda67a8f81285a6104e
KatiePee <piele.katie@gmail.com>
2023-08-04 13:02:40 -0700

    some styles

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -10,6 +10,7 @@ import './Navigation.css';
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 	const cart = useSelector(state => state.cart)
+	const user = useSelector(state => state.session.user)
 	const dispatch = useDispatch()
 	const [isLoading, setIsLoading] = useState(true);
 
@@ -25,21 +26,32 @@ function Navigation({ isLoaded }) {
 	if (isLoading) return <div>Loading...</div>;
 
 	return (
-		<div className='nav-bar'>
-			<NavLink className='nav-logo' exact to="/">🌿Plantsy</NavLink>
-			{isLoaded && (
-				<div className='nav-bar-buttons'>
-					<ProfileButton user={sessionUser} />
-					<div className='nav-bar__cart' onClick={() => alert('feature coming soon')}>
-						<i className="fa-solid fa-cart-shopping"></i>
-						{cart && (<div className='cart_bubble'>{cart.items.length}</div>)}
+		<div className='header'>
+			<div className='header__banner'><p>Welcome, {user ? user.firstName : 'to Plantsy'}</p></div>
+
+			<div className='nav-bar'>
+				<NavLink className='nav-logo' exact to="/">🌿Plantsy</NavLink>
+				<ul className='nav-cats'>
+					<li>Shop All</li>
+					<li>Plants</li>
+					<li>Care Tools</li>
+					<li>Pet Freindly</li>
+					<li>About</li>
+				</ul>
+				{isLoaded && (
+					<div className='nav-bar-buttons'>
+						<ProfileButton user={sessionUser} />
+						<div className='nav-bar__cart' onClick={() => alert('feature coming soon')}>
+							<i className="fa-solid fa-cart-shopping"></i>
+							{cart && (<div className='cart_bubble'>{cart.items.length}</div>)}
+						</div>
+						<OpenModalButton
+							buttonText='cart'
+							modalComponent={<CartModal />}
+						/>
 					</div>
-					<OpenModalButton
-						buttonText='cart'
-						modalComponent={<CartModal />}
-					/>
-				</div>
-			)}
+				)}
+			</div>
 		</div>
 	);
 }
